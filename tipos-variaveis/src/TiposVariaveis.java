625274ecc8d499c4ffe79a52c9841d5e1a22efc8
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2023-12-19 14:45:10 -0400

    tipos e variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioCurto = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+        System.out.println(numero);
+
+        double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+    }
+}
