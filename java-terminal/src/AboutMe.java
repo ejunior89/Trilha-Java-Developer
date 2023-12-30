3257a5621be96fea9b0a0e73394a605af25ec4a2
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2023-12-30 07:51:44 -0400

    uso de pacotes

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,14 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
 
     public static void main(String[] args) {
+        // criando o objeto scanner 
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args [2]);
-        double altura = Double.valueOf(args [3]);
+        System.out.println("Digite sua Altura");
+        double altura = scanner.nextDouble();
 
+        System.out.println("Digite seu Nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu Sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua Idade");
+        int idade = scanner.nextInt();
+
+        // fechando o scanner após a leitura
+        scanner.close();
+
+        // imprimindo os dados obtidos pelo usuario
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + "anos");
-        System.out.println("Minha altura é " + altura + "cm ");
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + " cm");
     }
 }
