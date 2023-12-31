4b09663c1fa4032012387683aeb4a95b3eac1073
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2023-12-31 08:12:29 -0400

    switch case

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,17 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "p";
+
+        if(sigla == "p")
+        System.out.println("PEQUENO");
+
+        else if(sigla == "M")
+        System.out.println("MEDIO");
+    
+        else if(sigla == "G")
+        System.out.print("GRANDE");
+
+        else
+        System.out.println("indefinido");
+}
+}
