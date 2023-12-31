d4d4be46c3d21e8606bd6a2ed5f5fc3ca550ec69
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2023-12-31 06:44:41 -0400

    .

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,17 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
         
-        int nota = 8;
+        int nota = 10;
 
-        if (nota >= 7) {
+        if (nota == 7) {
             System.out.println("Aprovado");
         }
+
+        else if (nota >= 5 && nota < 7)
+        System.out.println("Prova de recuperação");
+
+        else if (nota >= 5 && nota < 7)
+        System.out.println("Prova de recuperação");
 
         else
         System.out.println("Reprovado");
