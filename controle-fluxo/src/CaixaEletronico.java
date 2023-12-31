b43c1e2aa1188f1995ff36642318ac0e32dfbf86
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2023-12-31 06:35:20 -0400

    condicionais compostas e simples

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,11 +1,12 @@
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 26.0;
 
         if(valorSolicitado < saldo)
             saldo = saldo - valorSolicitado;
-
-        System.out.println(saldo);
+        
+        else
+        System.out.println("saldo insufficient");
 }
 }
