976c6e58ec7c9e9016c03445577e52aef24e068f
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2024-01-02 13:08:53 -0400

    uso de do while.

---
+++ controle-fluxo/src/ExemploDoWhile.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+
+        do {
+            // executando repetidas vezes até alguem atender
+            System.out.println("Telefone tocando");
+
+        }while(tocando());
+        System.out.println("Alô !!!");
+    }
+
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu? " + atendeu);
+        //negando o ato de continuar toando
+        return ! atendeu;
+    }
+}
