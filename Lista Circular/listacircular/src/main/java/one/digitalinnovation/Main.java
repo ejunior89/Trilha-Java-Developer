d2f1aefb1f62c71f30bdeedd5d71b2554ea25d89
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2024-01-16 11:41:36 -0400

    listas encadeadas duplamente

--- Lista Circular/listacircular/src/main/java/one/digitalinnovation/Main.java
+++ Lista Circular/listacircular/src/main/java/one/digitalinnovation/Main.java
@@ -2,33 +2,32 @@ package one.digitalinnovation;
 
 public class Main {
     public static void main(String[] args) {
-        ListaCircular<Integer> lista = new ListaCircular<>();
+        ListaCircular<String> listaComida = new ListaCircular<>();
 
         // Adiciona elementos à lista
-        lista.add(1);
-        lista.add(2);
-        lista.add(3);
-        lista.add(4);
-        lista.add(5);
-        lista.add(6);
+        listaComida.add("Arroz");
+        listaComida.add("Feijão");
+        listaComida.add("Carne");
+        listaComida.add("Peixe");
+        listaComida.add("Sabão");
 
         // Imprime a lista
-        System.out.println("Lista: " + lista.toString());
+        System.out.println("Lista de Comida: " + listaComida.toString());
 
         // Remove um elemento da lista
-        lista.remove(1);
+        listaComida.remove(1);
 
         // Imprime a lista após remoção
-        System.out.println("Lista após remoção: " + lista.toString());
+        System.out.println("Lista de Comida após remoção: " + listaComida.toString());
 
         // Obtém um elemento da lista
-        int elemento = lista.get(0);
+        String elemento = listaComida.get(0);
         System.out.println("Elemento na posição 0: " + elemento);
 
         // Imprime o tamanho da lista
-        System.out.println("Tamanho da lista: " + lista.size());
+        System.out.println("Tamanho da lista de Comida: " + listaComida.size());
 
         // Verifica se a lista está vazia
-        System.out.println("A lista está vazia? " + lista.isEmpty());
+        System.out.println("A lista de Comida está vazia? " + listaComida.isEmpty());
     }
 }
